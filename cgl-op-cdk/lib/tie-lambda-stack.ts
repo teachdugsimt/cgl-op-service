b19ac06e4da7a13f3296f153791cfb4527c989a6
tuntikorn <tuntikornwisuttikul@gmail.com>
2021-05-25 18:56:46 +0700

    build: cdk template

--- cgl-op-cdk/lib/tie-lambda-stack.ts
+++ cgl-op-cdk/lib/tie-lambda-stack.ts
@@ -5,7 +5,6 @@ import { LambdaAuthorizerStack } from './lambda-authorizer-stack/lambda-authoriz
 import { LambdaAuthenticationStack } from './lambda-authentication-stack/lambda-authentication-stack'
 import { LambdaTruckServiceStack } from './lambda-truck-service-stack/lambda-truck-service-stack'
 import { ApiGatewayStack } from './api-gateway-stack/api-gateway-stack'
-import { LambdaTestServiceStack } from './test-lambda-stack/lambda-test-service-stack'
 import * as apigateway from '@aws-cdk/aws-apigateway';
 
 export class TieLambdaStack extends cdk.Stack {
@@ -14,7 +13,6 @@ export class TieLambdaStack extends cdk.Stack {
   lambdaAuthorizerResources: LambdaAuthorizerStack
   lambdaAuthenticationResources: LambdaAuthenticationStack
   lambdaTruckServiceResources: LambdaTruckServiceStack
-  lambdaTestServiceResources: LambdaTestServiceStack
   // apiGatewayResources: ApiGatewayStack
 
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -23,15 +21,13 @@ export class TieLambdaStack extends cdk.Stack {
     this.lambdaLayerResources = new LambdaLayerStack(this, "legacy-lambda-layer-resources")
     const { layer } = this.lambdaLayerResources
 
-    // const apigatewayProps = new ApiGatewayStack(this, 'CglOpAPI', { deploy: true })
-    // const { apigw } = apigatewayProps
     const apigw = new apigateway.RestApi(this, 'CglOpAPI', { deploy: true })
 
-    // this.lambdaAuthorizerResources = new LambdaAuthorizerStack(this, "legacy-lambda-authorizer-resources", { apigw })
-    // const { authorizer } = this.lambdaAuthorizerResources
+    this.lambdaAuthorizerResources = new LambdaAuthorizerStack(this, "legacy-lambda-authorizer-resources", { apigw })
+    const { authorizer } = this.lambdaAuthorizerResources
 
-    // this.lambdaAuthenticationResources = new LambdaAuthenticationStack(this, "legacy-lambda-authentication-resources", { apigw, layer, authorizer })
-    // this.lambdaMessagingResources = new LambdaMessagingStack(this, "legacy-lambda-messaging-resources", { apigw })
+    this.lambdaAuthenticationResources = new LambdaAuthenticationStack(this, "legacy-lambda-authentication-resources", { apigw, layer, authorizer })
+    this.lambdaMessagingResources = new LambdaMessagingStack(this, "legacy-lambda-messaging-resources", { apigw })
     this.lambdaTruckServiceResources = new LambdaTruckServiceStack(this, "legacy-lambda-truck-service-resources", { apigw })
 
     // this.lambdaTruckServiceResources.addDependency(apigw)
