867f7766571d3ef609baba74a5cf5a5b9b8b6dec
tuntikorn <tuntikornwisuttikul@gmail.com>
2021-05-25 19:01:22 +0700

    build: update cdk templage

--- cgl-op-cdk/bin/ci.ts
+++
@@ -1,9 +0,0 @@
-// #!/usr/bin/env node
-// import "source-map-support/register"
-// import cdk = require("@aws-cdk/core")
-// import { CIStack } from "../lib/ci-stack/ci-stack"
-
-// const app = new cdk.App()
-// new CIStack(app, "CDKExampleLambdaApiCIStack", {
-//     repositoryName: "cgl-op-service",
-// })
