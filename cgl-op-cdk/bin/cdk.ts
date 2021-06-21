e4b20f445274147d278d7eff4dcebd9fba16cf64
tuntikorn <tuntikornwisuttikul@gmail.com>
2021-06-21 12:32:51 +0700

    chore: merge service

--- cgl-op-cdk/bin/cdk.ts
+++ cgl-op-cdk/bin/cdk.ts
@@ -2,7 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { TieLambdaStack } from '../lib/tie-lambda-stack';
-import { LambdaTestCloudFront } from '../lib/api-art-test/lambda-art-test-stack'
+// import { LambdaTestCloudFront } from '../lib/api-art-test/lambda-art-test-stack'
 
 const app = new cdk.App();
 
@@ -10,6 +10,6 @@ const secret_key = "CGLDevDbInstance"
 
 const envSgp = { region: 'ap-southeast-1' }
 new TieLambdaStack(app, 'CglOpServiceLambdaStack', { env: envSgp, secretKey: secret_key });
-new LambdaTestCloudFront(app, "TestMethodOptionsStack", { env: envSgp })
+// new LambdaTestCloudFront(app, "TestMethodOptionsStack", { env: envSgp })
 
 
